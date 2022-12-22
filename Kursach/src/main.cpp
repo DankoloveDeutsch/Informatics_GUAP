c6366e3bec2a28cc0a6c716fc5a6ec139480e217
Danko <reg.danko@mail.ru>
2022-12-22 05:11:22 +0300

    init

---
+++ Kursach/src/main.cpp
@@ -0,0 +1,245 @@
+﻿#include <iostream>
+#include <math.h>
+#include <cassert>
+
+class Gauss {
+private:
+    int n = 0, x_count;
+    float temp;
+    float** matrix_A;
+    float** vector_B;
+    float** matrix_answer;
+    float** y;
+    float** x;
+    double max_value;
+    int max_value_index;
+public:
+    Gauss() {
+        n = 3;
+        x_count = 0;
+    };
+    double f(int select, double x) {
+        switch (select)
+        {
+        case 0:
+            return 1;
+            break;
+        case 1:
+            return x;
+            break;
+        case 2:
+            return pow(exp(1), -1 * x);
+            break;
+        }
+        return 0.0;
+    }
+
+    void out_matrix(float** mat, int n, int m) {
+       
+        for (int i = 0; i < n; i++) {
+            std::cout << std::endl;
+            for (int j = 0; j < m; j++) {
+                std::cout << mat[i][j] << " ";
+            }
+        }
+        std::cout << std::endl;
+    }
+    float** get_matrix(int n, int m) {
+        float** matrix = new float* [n];
+        for (int i = 0; i < n; i++) {
+            matrix[i] = new float[m];
+        }
+        return matrix;
+    };
+    void sizematrix() {
+        bool flag = true;
+        std::cout << "Input count of X: ";
+        std::cin >> x_count;
+        x = get_matrix(x_count, 1);
+        y = get_matrix(x_count, 1);
+        for (int i = 0; i < x_count; i++) {
+            std::cout << "x[" << i << "]: ";
+            std::cin >> x[i][0];
+        }
+        for (int i = 0; i < x_count; i++) {
+            std::cout << "y[" << i << "]: ";
+            std::cin >> y[i][0];
+        }
+        matrix_A = get_matrix(n, n + 1);
+        vector_B = get_matrix(n, 1);
+        float phi_k, phi_l;
+        for (int k = 0; k < n; k++) {
+            for (int l = 0; l < n; l++) {
+                temp = 0;
+                for (int i = 0; i < x_count; i++) {
+                    phi_k = f(k, x[i][0]);
+                    phi_l = f(l, x[i][0]);
+                    temp += phi_k * phi_l;
+                }
+                matrix_A[k][l] = temp;
+            }
+        }
+
+        for (int k = 0; k < n; k++) {
+            temp = 0;
+            for (int i = 0; i < x_count; i++)
+                temp += y[i][0] * f(k, x[i][0]);
+            vector_B[k][0] = temp;
+        }
+        for (int i = 0; i < n; i++) {
+            matrix_A[i][n] = vector_B[i][0];
+        }
+    };
+    void matrix_up() {
+        std::cout << "\nMatrix A: " << std::endl;
+        out_matrix(matrix_A, n, n + 1);
+    }
+ 
+    void swap_rows(float** matrix, int n, int i, int j)
+    {
+
+        for (int k = 0; k <= n; k++)
+            std::swap(matrix[i][k], matrix[j][k]);
+    }
+
+    void averse() {
+        // Прямой ход Гаусса
+        temp = 0;
+
+        for (int k = 0; k < n; k++) {
+            max_value = abs(matrix_A[k][k]);
+            max_value_index = k;
+
+            for (int i = k + 1; i < n; i++)
+                if (abs(matrix_A[i][k]) > max_value) {
+                    max_value = abs(matrix_A[i][k]);
+                    max_value_index = i;
+                }
+
+            assert(matrix_A[max_value_index][k] != 0);
+
+            if (k != max_value_index) {
+                swap_rows(matrix_A, n, k, max_value_index);
+            }
+
+
+            for (int i = n; i >= k; i--) {
+                matrix_A[k][i] /= matrix_A[k][k];
+            }
+            for (int i = k + 1; i < n; i++) {
+                temp = matrix_A[i][k] / matrix_A[k][k];
+                for (int j = k + 1; j <= n; j++) {
+                    matrix_A[i][j] -= matrix_A[k][j] * temp;
+                }
+                matrix_A[i][k] = 0;
+            }
+           
+        }
+    };
+    void reverse() {
+        // Обратный ход Гаусса 
+        matrix_answer = get_matrix(n, 1);
+        matrix_answer[n - 1][0] = matrix_A[n - 1][n];
+        for (int i = n - 2; i >= 0; i--)
+        {
+            matrix_answer[i][0] = matrix_A[i][n];
+            for (int j = i + 1; j < n; j++) {
+                matrix_answer[i][0] -= matrix_A[i][j] * matrix_answer[j][0];
+            }
+        }
+        
+        
+    };
+    void matrix_answer_out() {
+        std::cout << "Matrix C\n";
+        out_matrix(matrix_answer, n, 1);
+    }
+
+
+    void output_statistic() {
+        float new_y, delta, max_delta, j = 0;
+        int max_delta_index;
+        
+        for (int i = 0; i < x_count; i++) {
+            new_y = 0;
+            for (int k = 0; k < n; k++) {
+                new_y += matrix_answer[k][0] * f(k, x[i][0]);
+            }
+            delta = fabs(new_y - y[i][0]);
+            if (i == 0) {
+                max_delta = delta;
+                max_delta_index = i;
+            }
+            if (max_delta < delta) {
+                max_delta = delta;
+                max_delta_index = i;
+            }
+            j += delta * delta;
+            std::cout << "new_y and delta: " << new_y << " " << delta << std::endl;
+        }
+        std::cout << "max_delta and X: " << max_delta << " " << x[max_delta_index][0];
+        std::cout << std::endl << "J = " << j << std::endl;
+    };
+
+    float get_alg_addition(float** mat, int n, int x, int y) {
+        float** algebric_addition = get_matrix(n - 1, n - 1);
+        int now_row = 0, now_column = 0;
+        for (int i = 0; i < n; i++) {
+            if (i == x) continue;
+            for (int j = 0; j < n; j++) {
+                if (j == y) continue;
+                algebric_addition[now_row][now_column] = mat[i][j];
+                now_column++;
+            }
+            now_column = 0;
+            now_row++;
+        }
+        float result = rec_determinant(algebric_addition, n - 1);
+        del_matrix(algebric_addition, n - 1);
+        if ((x + y) % 2 == 1) return -result;
+        else return result;
+    };
+    float rec_determinant(float** mat, int n) {
+        // рекурентное вычесление детерминанта
+        if (n == 1)
+            return mat[0][0];
+        if (n == 2)
+            return mat[0][0] * mat[1][1] - mat[1][0] * mat[0][1];
+        float determinant = 0;
+        for (int i = 0; i < n; i++) {
+            float result = get_alg_addition(mat, n, i, 0);
+            determinant += mat[i][0] * result;
+        }
+        return determinant;
+    };
+    void del_matrix(float** mat, int n) {
+        for (int i = 0; i < n; i++) {
+            delete[] mat[i];
+        }
+        delete[] mat;
+        mat = nullptr;
+    };
+ 
+    ~Gauss() {
+        del_matrix(matrix_A, n);
+        del_matrix(vector_B, n);
+        del_matrix(matrix_answer, n);
+        del_matrix(y, n);
+        del_matrix(x, n);
+    };
+};
+
+
+
+int main() {
+    Gauss* a = new Gauss;
+    a->sizematrix();
+    a->matrix_up();
+    a->averse();
+    a->reverse();
+    std::cout << std::endl;
+
+    a->matrix_answer_out();
+    std::cout << std::endl;
+    a->output_statistic();
+}
---
+++ Lab/OP_lab5/src/main.cpp
@@ -0,0 +1,342 @@
+#include <iostream>
+#include <chrono>
+#include <random>
+#include <cassert>
+#include <stdlib.h>
+
+template <typename T>
+class MyVector {
+private:
+    T* _array;
+    std::size_t _size;
+    std::size_t _allocated_memory;
+
+    void resize_memory(size_t new_size) {
+        if (_array == nullptr) {
+            _allocated_memory = 1;
+            _size = 1;
+            _array = new T[_allocated_memory];
+        }
+        if ((new_size * 2 > _allocated_memory) && (new_size <= _allocated_memory))
+            return;
+
+        size_t new_allocated_memory = new_size + new_size / 2;
+        T* new_array = new T[new_allocated_memory];
+        for (int i = 0; (i < _size) && (i < new_allocated_memory); i++)
+            new_array[i] = _array[i];
+
+        delete[] _array;
+        _array = new_array;
+        _allocated_memory = new_allocated_memory;
+        new_array = nullptr;
+    };
+public:
+    MyVector() {
+        _allocated_memory = 0;
+        _size = 0;
+        _array = nullptr;
+    };
+    MyVector(std::size_t size)
+    {
+        _allocated_memory = size + size / 2;
+        _array = new T[_allocated_memory];
+        _size = size;
+    };
+    MyVector(std::size_t size, T default_value)
+    {
+        for (int i = 0; i < _size; i++)
+            _array[i] = default_value;
+    };
+    ~MyVector() {
+        delete[] _array;
+    };
+
+    T& operator[] (std::size_t idx) {
+       
+        if (idx < 0) {
+            std::cout << "Error, idx<0" << std::endl;
+            exit;
+        }
+        if (idx > _size) {
+            std::cout << "Error,1 index>size Myvector" << std::endl << idx << std::endl;
+            exit;
+        }
+        return _array[idx];
+    };
+    std::size_t size() {
+        return _size;
+    };
+    std::size_t allocated_memory_size() {
+        return _allocated_memory;
+    };
+    void resize(std::size_t new_size) {
+        resize(new_size, T());
+    };
+    void resize(std::size_t new_size, T default_value)
+    {
+        std::size_t old_size = _size;
+        resize_memory(new_size);
+        for (int i = old_size; i < new_size; i++)
+            _array[i] = default_value;
+        _size = new_size;
+    };
+    bool empty() {
+        return _size == 0;
+    };
+    void push_back(T new_element) {
+        resize(_size + 1, new_element);
+    };
+    T pop_back() {
+        resize(_size - 1);
+        return _array[_size];
+    };
+    void insert(T new_element) {
+        insert(0, 1, new_element);
+    };
+    void insert(std::size_t start_index, std::size_t count_elements, T default_element) {
+        std::size_t old_size = _size;
+        resize(_size + count_elements);
+        for (int i = old_size - 1; i >= 0; i--) {
+            _array[i + count_elements] = _array[i];
+        }
+        for (int i = 0; i < count_elements; i++) {
+            _array[start_index + i] = default_element;
+        }
+    };
+    T front() {
+        assert(_size > 0);
+        return _array[0];
+    };
+    T back() {
+        assert(_size > 0);
+        return _array[_size - 1];
+    };
+    T erase(std::size_t idx) {
+        assert((idx >= 0) && (idx < _size) && (_size > 0));
+        resize(_size - 1);
+        T deleted_element = _array[idx];
+        for (int i = 0; i < _size; i++)
+        {
+            if (i >= idx)
+                _array[i] = _array[i + 1];
+        }
+        return deleted_element;
+    };
+    void clear() {
+        _size = 0;
+        _allocated_memory = 0;
+        delete[] _array;
+        _array = nullptr;
+    };
+    void swap_elements(std::size_t first_ind, std::size_t second_ind) {
+        T temp = _array[first_ind];
+        _array[first_ind] = _array[second_ind];
+        _array[second_ind] = temp;
+    };
+    void print_data() {
+        for (int i = 0; i < _size; i++)
+        {
+            std::cout << _array[i] << " ";
+        }
+        std::cout << std::endl;
+    };
+};
+
+int randint(int max_size)
+{
+    return std::rand() % max_size;
+}
+
+void generateTest(MyVector<int>& vec, std::size_t vector_size, int max_integer = 100)
+{
+    vec.clear();
+    vec.resize(vector_size);
+
+    int randi;
+    for (int i = 0; i < vector_size; i++) {
+        randi = randint(max_integer);
+        vec[i] = randi;
+    }
+}
+
+void stupid_sort(MyVector<int>& vec, std::size_t vector_size) {
+    int n,i,j;
+    for (j = 0; j<vector_size;j++){
+        for (i = 0; i < vector_size-1; i++) {
+            if (vec[i] > vec[i + 1]) {
+                n = vec[i];
+                vec[i] = vec[i + 1];
+                vec[i + 1] = n;
+            }
+        }
+    }
+}
+void stupid_sort_reverse(MyVector<int>& vec, std::size_t vector_size) {
+    int n, i, j;
+    for (j = 0; j < vector_size; j++) {
+        for (i = 0; i < vector_size - 1; i++) {
+            if (vec[i] < vec[i + 1]) {
+                n = vec[i];
+                vec[i] = vec[i + 1];
+                vec[i + 1] = n;
+            }
+        }
+    }
+}
+
+
+
+void quicksort(MyVector<int>& vector, int left, int right) {
+    int leftpoint=0, rightpoint=0, midpoint=0, piv=0;
+    leftpoint = left;
+    rightpoint = right;
+    midpoint = left + (right - left) / 2;
+    piv = vector[midpoint];
+    
+    int temp;
+    while (leftpoint<right || rightpoint>left) {
+        while (vector[leftpoint] < piv)
+            leftpoint++;
+        while (vector[rightpoint] > piv)
+            rightpoint--;
+
+        if (leftpoint <= rightpoint) {
+            temp = vector[leftpoint];
+            vector[leftpoint] = vector[rightpoint];
+            vector[rightpoint] = temp;
+            leftpoint++;
+            rightpoint--;
+        }
+        else {
+            if (left < right)
+                quicksort(vector, leftpoint, right);
+            if (rightpoint > left)
+                quicksort(vector, left, rightpoint);
+            return;
+        }
+    }
+}
+
+
+
+void quicksort_recursive(MyVector<int>& vector, int left, int right) {
+    
+    int temp,pivot=0,leftpoint=0,rightpoint=0;
+    
+    pivot = vector[right];
+    leftpoint = left - 1;
+    rightpoint = right;
+    bool f = true;
+    
+    if (left < right) {
+        while (f){
+            while (vector[++leftpoint] > pivot) {
+            }
+            while (rightpoint > 0 && vector[--rightpoint] < pivot) {
+            }
+            if (leftpoint >= rightpoint) {
+                f = false;
+            }
+            else {
+                temp = vector[leftpoint];
+                vector[leftpoint] = vector[rightpoint];
+                vector[rightpoint] = temp;
+            }
+        }
+       
+        temp = vector[leftpoint];
+        vector[leftpoint] = vector[right];
+        vector[right] = temp;
+
+        if (leftpoint>0){
+        quicksort_recursive(vector, left, leftpoint-1);
+        }
+        quicksort_recursive(vector, leftpoint +1, right);
+    }
+}
+
+
+void test_stupid_sort(int n) {
+    MyVector<int> vec_ideal;
+    MyVector<int> vec_random;
+    MyVector<int> vec_recursive;
+
+    generateTest(vec_ideal, n);
+    quicksort(vec_ideal, 0, n-1);
+    generateTest(vec_random, n);
+    generateTest(vec_recursive, n);
+    quicksort_recursive(vec_recursive, 0, n-1);
+  
+    
+    auto begin = std::chrono::steady_clock::now();
+    stupid_sort(vec_ideal, n);
+    auto end = std::chrono::steady_clock::now();
+    auto time_ideal = std::chrono::duration_cast<std::chrono::microseconds>(end - begin);
+
+    begin = std::chrono::steady_clock::now();
+    stupid_sort(vec_random, n);
+    end = std::chrono::steady_clock::now();
+    auto time_random = std::chrono::duration_cast<std::chrono::microseconds>(end - begin);
+
+    begin = std::chrono::steady_clock::now();
+    stupid_sort(vec_recursive, n);
+    end = std::chrono::steady_clock::now();
+    auto time_recursive = std::chrono::duration_cast<std::chrono::microseconds>(end - begin);
+
+    std::cout << "Stupid sort ideal (count elements " << n << ") " << time_ideal.count() << std::endl;
+    std::cout << "Stupid sort recursive (count elements " << n << ") " << time_recursive.count() << std::endl;
+    std::cout << "Stupid sort random (count elements " << n << ") " << time_random.count() << std::endl;
+    std::cout << std::endl;
+    
+}
+void test_quicksort(int n) {
+    MyVector<int> vec_ideal;
+    MyVector<int> vec_random;
+    MyVector<int> vec_recursive;
+
+    generateTest(vec_ideal, n);
+    quicksort(vec_ideal, 0, n - 1);
+    generateTest(vec_random, n);
+    generateTest(vec_recursive, n);
+    quicksort_recursive(vec_recursive, 0, n -1);
+
+    auto begin = std::chrono::steady_clock::now();
+    quicksort(vec_ideal,0, n-1);
+    auto end = std::chrono::steady_clock::now();
+    auto time_ideal = std::chrono::duration_cast<std::chrono::microseconds>(end - begin);
+
+    begin = std::chrono::steady_clock::now();
+    quicksort(vec_random,0, n-1);
+    end = std::chrono::steady_clock::now();
+    auto time_random = std::chrono::duration_cast<std::chrono::microseconds>(end - begin);
+
+    begin = std::chrono::steady_clock::now();
+    quicksort(vec_recursive,0, n-1);
+    end = std::chrono::steady_clock::now();
+    auto time_recursive = std::chrono::duration_cast<std::chrono::microseconds>(end - begin);
+
+    std::cout << "Quick sort ideal (count elements " << n << ") " << time_ideal.count() << std::endl;
+    std::cout << "Quick sort recursive (count elements " << n << ") " << time_recursive.count() << std::endl;
+    std::cout << "Quick sort random (count elements " << n << ") " << time_random.count() << std::endl;
+    std::cout << std::endl;
+
+}
+
+int main() {
+   
+    test_stupid_sort(100);
+    test_stupid_sort(1000);
+    test_stupid_sort(10000);
+    test_stupid_sort(20000);
+    test_quicksort(100);
+    test_quicksort(1000);
+    test_quicksort(10000);
+    test_quicksort(20000);
+   /*
+    test_quicksort(50000);
+    test_quicksort(100000);
+    test_quicksort(200000);
+    test_quicksort(1000000);
+    */
+   
+}
