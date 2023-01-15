a46712b432bd9cfa5dbd18d58b0d2547583cb794
Danko <reg.danko@mail.ru>
2023-01-15 21:34:35 +0300

    update

--- Lab/OP_lab5/src/main.cpp
+++ Lab/OP_lab5/src/main.cpp
@@ -51,7 +51,7 @@ public:
         delete[] _array;
     };
 
-    T& operator[] (std::size_t idx) {
+    T& operator[] (std::size_t idx) { // доступ по индексу
        
         if (idx < 0) {
             std::cout << "Error, idx<0" << std::endl;
@@ -63,13 +63,13 @@ public:
         }
         return _array[idx];
     };
-    std::size_t size() {
+    std::size_t size() { // размерность
         return _size;
     };
     std::size_t allocated_memory_size() {
         return _allocated_memory;
     };
-    void resize(std::size_t new_size) {
+    void resize(std::size_t new_size) { // изменить размер
         resize(new_size, T());
     };
     void resize(std::size_t new_size, T default_value)
@@ -80,22 +80,22 @@ public:
             _array[i] = default_value;
         _size = new_size;
     };
-    bool empty() {
+    bool empty() { // пуст ли контейнер
         return _size == 0;
     };
-    void push_back(T new_element) {
+    void push_back(T new_element) { // ƒобавить в конец
         resize(_size + 1, new_element);
     };
-    T pop_back() {
+    T pop_back() { // ”далить с конца
         resize(_size - 1);
         return _array[_size];
     };
-    void insert(T new_element) {
+    void insert(T new_element) { // добавить в начало
         insert(0, 1, new_element);
     };
-    void insert(std::size_t start_index, std::size_t count_elements, T default_element) {
+    void insert(std::size_t start_index, std::size_t count_elements, T default_element) { // вставить n элементов начина€ с позиции start index
         std::size_t old_size = _size;
-        resize(_size + count_elements);
+        resize(_size + count_elements); 
         for (int i = old_size - 1; i >= 0; i--) {
             _array[i + count_elements] = _array[i];
         }
@@ -103,15 +103,15 @@ public:
             _array[start_index + i] = default_element;
         }
     };
-    T front() {
+    T front() {// перва€ €чейка вектора
         assert(_size > 0);
         return _array[0];
     };
-    T back() {
+    T back() { // последн€€ €чейка вектора
         assert(_size > 0);
         return _array[_size - 1];
     };
-    T erase(std::size_t idx) {
+    T erase(std::size_t idx) { // ”далить дин элемент вектора
         assert((idx >= 0) && (idx < _size) && (_size > 0));
         resize(_size - 1);
         T deleted_element = _array[idx];
@@ -122,7 +122,7 @@ public:
         }
         return deleted_element;
     };
-    void clear() {
+    void clear() { // удалить все элементы вектора
         _size = 0;
         _allocated_memory = 0;
         delete[] _array;
@@ -159,39 +159,47 @@ void generateTest(MyVector<int>& vec, std::size_t vector_size, int max_integer =
     }
 }
 
-void stupid_sort(MyVector<int>& vec, std::size_t vector_size) {
-    int n,i,j;
+void stupid_sort(MyVector<int>& vec, std::size_t vector_size, int n = 0) {
+    int temp,i,j,l=0;
     for (j = 0; j<vector_size;j++){
         for (i = 0; i < vector_size-1; i++) {
             if (vec[i] > vec[i + 1]) {
-                n = vec[i];
+                
+                temp = vec[i];
                 vec[i] = vec[i + 1];
-                vec[i + 1] = n;
+                vec[i + 1] = temp;
+                l++;
             }
         }
     }
+    if (n == 1) {
+        std::cout << "Count perestan stupid sort (count elements " << vec.size() << ") " << l << std::endl;
+    }
 }
 void stupid_sort_reverse(MyVector<int>& vec, std::size_t vector_size) {
-    int n, i, j;
+    int n, i, j,l=0;
     for (j = 0; j < vector_size; j++) {
         for (i = 0; i < vector_size - 1; i++) {
             if (vec[i] < vec[i + 1]) {
                 n = vec[i];
                 vec[i] = vec[i + 1];
                 vec[i + 1] = n;
+                
             }
         }
     }
+    
 }
 
 
 
-void quicksort(MyVector<int>& vector, int left, int right) {
+void quicksort(MyVector<int>& vector, int left, int right,int n=0) {
     int leftpoint=0, rightpoint=0, midpoint=0, piv=0;
     leftpoint = left;
     rightpoint = right;
     midpoint = left + (right - left) / 2;
     piv = vector[midpoint];
+    int l = 0;
     
     int temp;
     while (leftpoint<right || rightpoint>left) {
@@ -206,15 +214,21 @@ void quicksort(MyVector<int>& vector, int left, int right) {
             vector[rightpoint] = temp;
             leftpoint++;
             rightpoint--;
+            l++;
+            
         }
         else {
             if (left < right)
                 quicksort(vector, leftpoint, right);
             if (rightpoint > left)
                 quicksort(vector, left, rightpoint);
+            if (n == 1) {
+                std::cout << "Count perestan quick sort (count elements " << vector.size() << ") " << l << std::endl;
+            }
             return;
         }
     }
+    
 }
 
 
@@ -256,7 +270,7 @@ void quicksort_recursive(MyVector<int>& vector, int left, int right) {
 }
 
 
-void test_stupid_sort(int n) {
+void test_stupid_sort(int n,int k=0) {
     MyVector<int> vec_ideal;
     MyVector<int> vec_random;
     MyVector<int> vec_recursive;
@@ -269,27 +283,29 @@ void test_stupid_sort(int n) {
   
     
     auto begin = std::chrono::steady_clock::now();
-    stupid_sort(vec_ideal, n);
+    stupid_sort(vec_ideal, n,k);
     auto end = std::chrono::steady_clock::now();
     auto time_ideal = std::chrono::duration_cast<std::chrono::microseconds>(end - begin);
 
-    begin = std::chrono::steady_clock::now();
-    stupid_sort(vec_random, n);
-    end = std::chrono::steady_clock::now();
-    auto time_random = std::chrono::duration_cast<std::chrono::microseconds>(end - begin);
+    std::cout << "Stupid sort ideal (count elements " << n << ") " << time_ideal.count() << std::endl<<std::endl;
 
     begin = std::chrono::steady_clock::now();
-    stupid_sort(vec_recursive, n);
+    stupid_sort(vec_recursive, n, k);
     end = std::chrono::steady_clock::now();
     auto time_recursive = std::chrono::duration_cast<std::chrono::microseconds>(end - begin);
 
-    std::cout << "Stupid sort ideal (count elements " << n << ") " << time_ideal.count() << std::endl;
-    std::cout << "Stupid sort recursive (count elements " << n << ") " << time_recursive.count() << std::endl;
-    std::cout << "Stupid sort random (count elements " << n << ") " << time_random.count() << std::endl;
+    std::cout << "Stupid sort recursive (count elements " << n << ") " << time_recursive.count() << std::endl << std::endl;
+
+    begin = std::chrono::steady_clock::now();
+    stupid_sort(vec_random, n,k);
+    end = std::chrono::steady_clock::now();
+    auto time_random = std::chrono::duration_cast<std::chrono::microseconds>(end - begin);
+
+    std::cout << "Stupid sort random (count elements " << n << ") " << time_random.count() << std::endl << std::endl;
     std::cout << std::endl;
     
 }
-void test_quicksort(int n) {
+void test_quicksort(int n,int k=0) {
     MyVector<int> vec_ideal;
     MyVector<int> vec_random;
     MyVector<int> vec_recursive;
@@ -301,42 +317,46 @@ void test_quicksort(int n) {
     quicksort_recursive(vec_recursive, 0, n -1);
 
     auto begin = std::chrono::steady_clock::now();
-    quicksort(vec_ideal,0, n-1);
+    quicksort(vec_ideal,0, n-1,k);
     auto end = std::chrono::steady_clock::now();
     auto time_ideal = std::chrono::duration_cast<std::chrono::microseconds>(end - begin);
 
-    begin = std::chrono::steady_clock::now();
-    quicksort(vec_random,0, n-1);
-    end = std::chrono::steady_clock::now();
-    auto time_random = std::chrono::duration_cast<std::chrono::microseconds>(end - begin);
+    std::cout << "Quick sort ideal (count elements " << n << ") " << time_ideal.count() << std::endl << std::endl;
 
     begin = std::chrono::steady_clock::now();
-    quicksort(vec_recursive,0, n-1);
+    quicksort(vec_recursive, 0, n - 1, k);
     end = std::chrono::steady_clock::now();
     auto time_recursive = std::chrono::duration_cast<std::chrono::microseconds>(end - begin);
 
-    std::cout << "Quick sort ideal (count elements " << n << ") " << time_ideal.count() << std::endl;
-    std::cout << "Quick sort recursive (count elements " << n << ") " << time_recursive.count() << std::endl;
-    std::cout << "Quick sort random (count elements " << n << ") " << time_random.count() << std::endl;
+    std::cout << "Quick sort recursive (count elements " << n << ") " << time_recursive.count() << std::endl << std::endl;
+
+    begin = std::chrono::steady_clock::now();
+    quicksort(vec_random,0, n-1,k);
+    end = std::chrono::steady_clock::now();
+    auto time_random = std::chrono::duration_cast<std::chrono::microseconds>(end - begin);
+
+    std::cout << "Quick sort random (count elements " << n << ") " << time_random.count() << std::endl << std::endl;
     std::cout << std::endl;
 
 }
 
 int main() {
    
-    test_stupid_sort(100);
-    test_stupid_sort(1000);
-    test_stupid_sort(10000);
-    test_stupid_sort(20000);
-    test_quicksort(100);
-    test_quicksort(1000);
-    test_quicksort(10000);
-    test_quicksort(20000);
-   /*
+    
+    
+    test_stupid_sort(100,1);
+    test_stupid_sort(1000,1);
+    test_stupid_sort(10000,1);
+    //test_stupid_sort(20000);
+    test_quicksort(100,1);
+    test_quicksort(1000,1);
+    test_quicksort(10000,1);
+    //test_quicksort(20000);
+    /*
     test_quicksort(50000);
     test_quicksort(100000);
     test_quicksort(200000);
     test_quicksort(1000000);
-    */
+   */
    
 }
